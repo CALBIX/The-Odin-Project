2e2a1d32dd4a5a424e40281ff6da0ce8fabd0977
Cailen <cailenb777@gmail.com>
2024-11-21 13:59:37 +0200

    Reworked getComputerChoice and getHumanChoice

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,18 +1,20 @@
+//Create a variable to store humans score
+var humanScore = 0;
+//Create a variable to store computer score
+var computerScore = 0;
+
 function getComputerChoice() {
     let randomNumber = Math.random()
+    let computerChoiceAssignment = "";
     if (randomNumber > 0.66) {
-        console.log("Rock")
+        computerChoiceAssignment = ("Rock")
     } else if (randomNumber <= 0.66 && randomNumber >= 0.33) {
-        console.log("Paper")
-    } else (console.log("Scissors"))
-    return randomNumber
+        computerChoiceAssignment = ("Paper")
+    } else computerChoiceAssignment = ("Scissors")
+    return console.log(computerChoiceAssignment.toLowerCase())
 }
-
-console.log(getComputerChoice())
 
 function getHumanChoice() {
-    prompt("Rock, Paper, or Scissors?")
-    return
+    let humanChoiceAssignment = prompt("Rock, Paper, or Scissors?")
+    return console.log(humanChoiceAssignment.toLowerCase())
 }
-
-console.log(getHumanChoice())
